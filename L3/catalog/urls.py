78f951f904a9e60f2c0bd7fdebc79e355742e903
vladenachescu <vlad.enachescu.2006@gmail.com>
2026-06-19 18:45:29 +0300

    Include accounts authentication urls in L3 catalog/urls.py

--- L3/catalog/urls.py
+++ L3/catalog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('courses.urls')),
 ]
+
